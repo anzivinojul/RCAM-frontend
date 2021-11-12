1d7162f25f6610d076fa064e661cf732a0b96e9b
anzivinojul <anzivinojulien@gmail.com>
2021-11-12 16:22:56 +0100

    add title login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -10,17 +10,20 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   login: boolean | undefined;
+  loginTitle: string | undefined;
 
   showConnexion() {
     this.login = true;
+    this.loginTitle = 'Connexion'
   }
 
   showInscription() {
     this.login = false;
+    this.loginTitle = 'Inscription'
   }
 
   ngOnInit(): void {
-    this.login = true;
+    this.showConnexion();
   }
 
 }
