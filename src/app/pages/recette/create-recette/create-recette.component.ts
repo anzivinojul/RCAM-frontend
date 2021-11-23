fb04fb14dda2bbd851c071f6ee477532e3789ecc
anzivinojul <anzivinojulien@gmail.com>
2021-11-23 16:32:36 +0100

    add create-recette [WIP]

--- src/app/pages/recette/create-recette/create-recette.component.ts
+++ src/app/pages/recette/create-recette/create-recette.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CategoryService } from 'src/app/core/api/category/category.service';
+import { Category } from 'src/app/interface/category';
 
 @Component({
   selector: 'app-create-recette',
@@ -7,9 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateRecetteComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    protected categoryService: CategoryService,
+  ) { }
+
+  categories!: Array<Category>;
+  filterCategoryRecette!: string;
+
+  imgURI!: string;
+
+  newRecetteForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    category: new FormControl('', [Validators.required]),
+    file: new FormControl('', [Validators.required]),
+    fileSource: new FormControl('', [Validators.required])
+  });
 
   ngOnInit(): void {
+    this.getCategories();
+  }
+
+  async getCategories() {
+    await this.categoryService.getCategories()
+      .toPromise()
+      .then((categories: any) => {
+        this.categories = categories;
+      })
+  }
+
+  onFileChange(event: any) {
+    const reader = new FileReader();
+
+    if(event.target.files && event.target.files.length) {
+
+      const [file] = event.target.files;
+
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+
+        this.imgURI = reader.result as string;
+
+        this.newRecetteForm.patchValue({
+          fileSource: reader.result
+        });
+
+      };
+
+    }
   }
 
 }
